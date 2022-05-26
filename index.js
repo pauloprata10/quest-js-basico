08b217970ac3ab7f287aec5e3cae4c795dd643be
Paulo Prata <pauloprata10@gmail.com>
2022-05-26 11:16:27 -0300

    alterando variaveis de let para const

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
 // Soma
 function soma(number1, number2){
-  let soma = number1 + number2
+  const soma = number1 + number2
   return 'A soma dos dois números é: ' +soma
 }
 
-let resultadoDaSoma = soma(5, 5)
+const resultadoDaSoma = soma(5, 5)
   console.log(resultadoDaSoma)
 
 
 // Divisão
 function divisao(number1, number2){
-  let divisao = number1 / number2
+  const divisao = number1 / number2
   return 'A divisão dos dois números é: '+divisao
 }
 
-let resultadoDaDivisao = divisao(20, 2)
+const resultadoDaDivisao = divisao(20, 2)
 console.log(resultadoDaDivisao)
 
 
 // Multiplicação
 function multiplicacao(number1, number2){
-  let multiplicacao = number1 * number2
+  const multiplicacao = number1 * number2
   return 'A multiplicação dos dois números é: '+multiplicacao
 }
 
-let resultadoDaMultiplicacao = multiplicacao(5, 2)
+const resultadoDaMultiplicacao = multiplicacao(5, 2)
 console.log(resultadoDaMultiplicacao)
 
 
 // Subtração
 function subtracao(number1, number2){
-  let subtracao = number1 - number2
+  const subtracao = number1 - number2
   return 'A subtração dos dois números é:' +subtracao
 }
 
-let resultadoDaSubtracao = subtracao(20,10)
+const resultadoDaSubtracao = subtracao(20,10)
 console.log(resultadoDaSubtracao)
